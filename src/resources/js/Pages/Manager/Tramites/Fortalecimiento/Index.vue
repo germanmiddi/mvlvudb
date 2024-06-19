de4e0320b352309c24d31796c45b0629de38ce57
Skieba Hector <skiebah@gmail.com>
2024-06-19 00:29:10 -0300

    Semodifica estilo de boton cancelar importar en fortalecimiento

--- src/resources/js/Pages/Manager/Tramites/Fortalecimiento/Index.vue
+++ src/resources/js/Pages/Manager/Tramites/Fortalecimiento/Index.vue
@@ -24,7 +24,7 @@
                     class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:order-1 sm:ml-3">Template</a>
                 
                 <a @click="formImport=false"
-                        class="order-0 inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:order-1 sm:ml-3">Cancelar</a>
+                        class="order-0 inline-flex items-center px-4 py-2 border border-red-700 shadow-sm text-sm font-medium rounded-md text-red-700 hover:text-white hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:order-1 sm:ml-3">Cancelar</a>
             </div>
             <div v-else class="mt-4 flex sm:mt-0 sm:ml-4">
                 <a :href="route('fortalecimiento.create')"

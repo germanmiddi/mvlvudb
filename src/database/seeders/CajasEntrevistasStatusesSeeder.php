122f269be16df00c0e3579510f5931edb2192fd6
German Middi <german.middi@gmail.com>
2025-01-13 11:40:25 +1100

    seeder status entrevistas

---
+++ src/database/seeders/CajasEntrevistasStatusesSeeder.php
@@ -0,0 +1,38 @@
+<?php   
+
+namespace Database\Seeders;
+
+use App\Models\Manager\CajasEntrevistasStatus;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CajasEntrevistasStatusesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['id' => 1,  'nombre' => 'PENDIENTE', 'descripcion' => 'Entrevista pendiente' ],
+            ['id' => 2,  'nombre' => 'APROBADA', 'descripcion' => 'Entrevista aprobada' ],
+            ['id' => 3,  'nombre' => 'RECHAZADA', 'descripcion' => 'Entrevista rechazada' ]
+        ];
+
+
+        foreach ($data as $item) {
+            CajasEntrevistasStatus::updateOrCreate(
+                ['id' => $item['id']],
+                [
+                    'nombre' => $item['nombre'],
+                    'descripcion' => $item['descripcion']
+                ]
+            );
+        }
+
+    }
+
+}

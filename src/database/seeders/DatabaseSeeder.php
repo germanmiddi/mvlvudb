c08bb404ee8559e1453b98b30f3842b4a2894c3b
CapanoCarola <130514395+CapanoCarola@users.noreply.github.com>
2025-01-23 14:41:50 -0300

    Agregar Seeders al DatabaseSeeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,45 +14,67 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ActividadesCbjSeeder::class
-            AcompanamientosCbjSeeder::class
-            BarriosSeeder::class
-            CargosAutoridadesSeeder::class
-            CanalAtencionSeeder::class
-            CentrosSaludSeeder::class
-            CategoriesSeeder::class
-            CoberturaMedicaSeeder::class
-            ComedoresSeeder::class
-            DependenciasSeeder::class
-            EstadoCbiSeeder::class
-            EstadoCbjSeeder::class
-            EstadoEducativoSeeder::class
-            EstadoGabineteSeeder::class
-            EstadosSaludSeeder::class
-            EscuelasSeeder::class
-            EscuelasDependenciaSeeder::class
-            EscuelasNivelesSeeder::class
-            EscuelasOrientacionesSeeder::class
-            EscuelasTurnosSeeder::class
-            ModalidadAtencionSeeder::class
-            NivelEducativoSeeder::class
-            PaisesSeeder::class
-            ParentescosSeeder::class
-            ProgramaSocialSeeder::class
-            RoleSeeder::class
-            RolTramiteSeeder::class
-            SituacionConyugalSeeder::class
-            SedesSeeder::class
-            TipoDocumentoSeeder::class
-            TipoOcupacionSeeder::class
-            TipoPensionSeeder::class
-            TipoTramiteSeeder::class
-            TipoVinculoFamiliarSeeder::class
-            TipoViviendaSeeder::class
-            TiposActividadesSeeder::class
-            TiposEntidadesSeeder::class
-            TramiteEstadosSeeder::class
-            UsersMvlSeeder::class
+            LocalidadesSeeder::class,
+            BarriosSeeder::class,
+            BarriosUpdateSeeder::class,
+            CargosAutoridadesSeeder::class,
+            CanalAtencionSeeder::class,
+            CanalAtencionMayoresSeeder::class,
+            CentrosSaludSeeder::class,
+            DependenciasSeeder::class,
+            CategoriesSeeder::class,
+            CoberturaMedicaSeeder::class,
+            ComedoresSeeder::class,
+            ActividadesCbjSeeder::class,
+            ActividadesCBSeeder::class,
+            AcompanamientosCbjSeeder::class,
+            EstadoCbiSeeder::class,
+            EstadoCbjSeeder::class,
+            EstadoEducativoSeeder::class,
+            EstadoGabineteSeeder::class,
+            EstadosSaludSeeder::class,
+            EscuelasSeeder::class,
+            EscuelasDependenciaSeeder::class,
+            EscuelasNivelesSeeder::class,
+            EscuelasOrientacionesSeeder::class,
+            EscuelasTurnosSeeder::class,
+            ModalidadAtencionSeeder::class,
+            NivelEducativoSeeder::class,
+            PaisesSeeder::class,
+            ParentescosSeeder::class,
+            ProgramaSocialSeeder::class,
+            RoleSeeder::class,
+            RolTramiteSeeder::class,
+            SituacionConyugalSeeder::class,
+            SedesSeeder::class,
+            TipoDocumentoSeeder::class,
+            TipoOcupacionSeeder::class,
+            TipoPensionSeeder::class,
+            TipoTramiteSeeder::class,
+            TipoVinculoFamiliarSeeder::class,
+            TipoViviendaSeeder::class,
+            TiposActividadesSeeder::class,
+            TiposEntidadesSeeder::class,
+            TramiteEstadosSeeder::class,
+            TiposLegajoCbSeeder::class,
+            UsersMvlSeeder::class,
+            DependenciasRolPrefixSeeder::class,
+            TipoTramiteCBSeeder::class,
+            TipoDerivacionesSeeder::class,
+            TipoArticulacionesSeeder::class,
+            ProgramasSocialesCBSeeder::class,
+            OrigenDemandasSeeder::class,
+            MotivoConsultaSeeder::class,
+            MotivoBajasSeeder::class,
+            FamiliarAntecedentesSeeder::class,
+            EstadoInformeCBSeeder::class,
+            EstadoActividadCBSeeder::class,
+            ColumnExportSeeder::class,
+            EstadoProgramaSocialCBSeeder::class,
+            TipoTratamientosSeeder::class,
+            TipoEnfermedadesSeeder::class,
+            EstadoGabineteCBSeeder::class,
+            CajasEntrevistasStatusesSeeder::class,
         ]);
 
         User::create([
@@ -60,6 +82,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'g@gmail.com',
             'password' => bcrypt('Inicio123')
         ]);
-        
+
     }
 }

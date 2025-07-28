2fac54b244eeaa44ecb536da9438215c49484ba6
German Middi <german.middi@gmail.com>
2025-07-29 00:39:54 +1000

    .

--- src/database/seeders/RoleCajasProfSeeder.php
+++ src/database/seeders/RoleCajasProfSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class RoleCajasSeeder extends Seeder
+class RoleCajasProfSeeder extends Seeder
 {
     /**
      * Run the database seeds.
